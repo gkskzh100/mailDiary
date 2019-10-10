169bc5aedf7dc79d0e3b7a799fa7dbb6b7b32abd
gkskzh100 <a22569447@gmail.com>
2019-10-10 20:48:22 +0900

    test

---
+++ app/src/main/java/com/diary/jimin/maildiary/MainActivity.java
@@ -0,0 +1,20 @@
+package com.diary.jimin.maildiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalendarView mainCalendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
