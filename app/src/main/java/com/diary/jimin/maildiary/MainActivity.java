be58f7a8b19da44808ad13fbc4c33d03a18d7a3f
gkskzh100 <a22569447@gmail.com>
2019-10-11 20:47:52 +0900

    main Add Intent
    WriteDiary Intent

--- app/src/main/java/com/diary/jimin/maildiary/MainActivity.java
+++ app/src/main/java/com/diary/jimin/maildiary/MainActivity.java
@@ -2,8 +2,11 @@ package com.diary.jimin.maildiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -18,6 +21,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private MaterialCalendarView mainCalendarView;
+    private Button mainWriteDiaryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainCalendarView = findViewById(R.id.main_calendar);
+        mainWriteDiaryBtn = findViewById(R.id.main_write_diary_btn);
+
+        mainWriteDiaryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WriteDiary.class);
+                startActivity(intent);
+            }
+        });
 
         Date currentTime = Calendar.getInstance().getTime();
         SimpleDateFormat dayFormat = new SimpleDateFormat("dd", Locale.getDefault());
