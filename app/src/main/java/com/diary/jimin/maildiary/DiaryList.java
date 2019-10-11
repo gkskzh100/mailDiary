2685e97502a8f9ea7a086d6f35c3d5b085a7d353
anuu0916 <youjin1939@gmail.com>
2019-10-11 22:08:53 +0900

    recycle

---
+++ app/src/main/java/com/diary/jimin/maildiary/DiaryList.java
@@ -0,0 +1,14 @@
+package com.diary.jimin.maildiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiaryList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_list);
+    }
+}
