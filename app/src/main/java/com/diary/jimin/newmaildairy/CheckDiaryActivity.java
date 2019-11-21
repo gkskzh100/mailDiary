a82300dc4f1cc0963ab8d8acc0eea055e0e74ef7
gkskzh100 <a22569447@gmail.com>
2019-11-21 14:52:12 +0900

    수정해놈

---
+++ app/src/main/java/com/diary/jimin/newmaildairy/CheckDiaryActivity.java
@@ -0,0 +1,16 @@
+package com.diary.jimin.newmaildairy;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheckDiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_diary);
+    }
+}
