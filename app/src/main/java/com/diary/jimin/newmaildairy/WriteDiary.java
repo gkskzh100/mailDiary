b26f1d9490c4bb302b1eb4a1ae261ae1571932e8
anuu0916 <youjin1939@gmail.com>
2019-11-21 23:38:53 +0900

    writeDiaryXML
    화면비율 수정
    감정 선택 전환 happy만 해놓음

--- app/src/main/java/com/diary/jimin/newmaildairy/WriteDiary.java
+++ app/src/main/java/com/diary/jimin/newmaildairy/WriteDiary.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,29 +23,60 @@ import java.util.Calendar;
 
 public class WriteDiary extends AppCompatActivity implements View.OnClickListener {
     EditText editDiary;
-    Button btnSave;
-    TextView tvDatePick;
+    Button SaveBtn;
+    TextView DatePickTV;
+    FrameLayout EmoHappyFL;
+    ImageView SelectIV, UnselectIV;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_diary);
 
+        //findViewById
         editDiary = findViewById(R.id.edit_diary);
-        btnSave = findViewById(R.id.btn_Save);
-        tvDatePick = findViewById(R.id.viewDatePick);
+        SaveBtn = findViewById(R.id.btn_Save);
+        DatePickTV = findViewById(R.id.viewDatePick);
+        EmoHappyFL = findViewById(R.id.write_diary_happy);
+        SelectIV = findViewById(R.id.happy_select_btn);
+        UnselectIV = findViewById(R.id.happy_unselect_btn);
 
+        //select 이미지 visiblity
+        UnselectIV.setVisibility(View.VISIBLE);
+        SelectIV.setVisibility(View.INVISIBLE);
+
+        //select 이미지 전환
+        EmoHappyFL.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                i = 1 - i;
+
+                if(i == 0){
+                    SelectIV.setVisibility(View.VISIBLE);
+                    UnselectIV.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    SelectIV.setVisibility(View.INVISIBLE);
+                    UnselectIV.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        //날짜 출력
         Calendar c = Calendar.getInstance();
         int cYear = c.get(Calendar.YEAR);
         int cMonth = c.get(Calendar.MONTH)+1;
         int cDay = c.get(Calendar.DAY_OF_MONTH);
 
-        tvDatePick.setText(cYear + "년 " + cMonth + "월 " + cDay + "일");
+        DatePickTV.setText(cYear + "년 " + cMonth + "월 " + cDay + "일");
     }
 
 
     @Override
     public void onClick(View v){
+        //일기 저장 (지금 안됨)
         switch (v.getId()){
             case R.id.btn_Save:
                 try{
