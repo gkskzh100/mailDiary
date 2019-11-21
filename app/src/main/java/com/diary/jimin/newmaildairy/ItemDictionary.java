3974c384fda5361da2ed3bd2acbb0e36df8438da
gkskzh100 <a22569447@gmail.com>
2019-11-21 20:58:57 +0900

    collectDiaryActivity v0.2
    Adapter 만드는중

--- app/src/main/java/com/diary/jimin/newmaildairy/ItemDictionary.java
+++ app/src/main/java/com/diary/jimin/newmaildairy/ItemDictionary.java
@@ -5,4 +5,36 @@ import android.widget.ImageView;
 public class ItemDictionary {
 
     private ImageView emoji;
+    private String date;
+    private String content;
+
+    public ItemDictionary(ImageView emoji, String date, String content) {
+        this.emoji = emoji;
+        this.date = date;
+        this.content = content;
+    }
+
+    public ImageView getEmoji() {
+        return emoji;
+    }
+
+    public void setEmoji(ImageView emoji) {
+        this.emoji = emoji;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
