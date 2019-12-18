11c086e5d1e06d45956024c1be9ed10f13aa3c39
jungmiin <jungmin0462@gmail.com>
2019-12-19 02:08:33 +0900

    편지 보기, 확인 액티비티 수정
    DB에서 읽어올 수 있게함

--- app/src/main/java/com/diary/jimin/newmaildairy/CheckLetterActivity.java
+++ app/src/main/java/com/diary/jimin/newmaildairy/CheckLetterActivity.java
@@ -2,7 +2,9 @@ package com.diary.jimin.newmaildairy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,7 +21,32 @@ public class CheckLetterActivity extends AppCompatActivity {
 
         init();
 
+        String extras;
+        Intent intent = getIntent();
+
+        extras = intent.getStringExtra("dateStr");
+        checkLetterDate.setText(extras);
+        Log.d("dateStr","checkLetterActivity = "+extras);
+
+        extras = intent.getStringExtra("LetterStr");
+        checkLetterContent.setText(extras);
+
+        extras = intent.getStringExtra("emojiStr");
+        if(extras.equals("angry"))
+            checkLetterImg.setImageResource(R.drawable.emo_diary_angry);
+        else if(extras.equals("cry"))
+            checkLetterImg.setImageResource(R.drawable.emo_diary_cry);
+        else if(extras.equals("good"))
+            checkLetterImg.setImageResource(R.drawable.emo_diary_good);
+        else if(extras.equals("happy"))
+            checkLetterImg.setImageResource(R.drawable.emo_diary_happy);
+        else if(extras.equals("sad"))
+            checkLetterImg.setImageResource(R.drawable.emo_diary_sad);
+        else if(extras.equals("soso"))
+            checkLetterImg.setImageResource(R.drawable.emo_diary_soso);
+
     }
+
 
     private void init(){
         checkLetterImg = findViewById(R.id.check_letter_emoji);
--- app/src/main/java/com/diary/jimin/newmaildairy/ViewLetterRecyclerAdapter.java
+++ app/src/main/java/com/diary/jimin/newmaildairy/ViewLetterRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,42 +15,125 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ViewLetterRecyclerAdapter extends RecyclerView.Adapter<ViewLetterRecyclerAdapter.ViewHolder>{
-    private ArrayList<String> mData;
+    private ArrayList<ItemDictionary> mData;
 
-    private RecycleAdapter.OnItemClickListener mListener = null;
+    private OnItemClickListener mListener = null;
 
     private FirebaseFirestore db;
     private FirebaseUser firebaseUser;
     private String userId;
 
+    private Context mContext;
+
+    public void setOnItemClickListener (OnItemClickListener listener) {
+        this.mListener =  listener;
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(View v, int position);
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView date;
+        protected ImageView emoji;
+        protected TextView kordate;
+        protected TextView date;
+        protected TextView content;
 
         public ViewHolder (View view){
             super(view);
-            this.date=(TextView) view.findViewById(R.id.view_letter_text);
+
+            this.kordate=(TextView) view.findViewById(R.id.view_letter_text);
+            this.content = itemView.findViewById(R.id.item_content_text);
+            this.emoji = itemView.findViewById(R.id.item_emo_image);
+            Log.d(this.getClass().getName(),"DB1");
 
             firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
             if (firebaseUser != null) {
                 userId = firebaseUser.getUid();
             }
-            //편지 보기 연결하기 !
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View view) {
+                    Log.d(this.getClass().getName(),"DB2");
+
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+
+                        Log.d(this.getClass().getName(),""+mListener);
+
+                        /** 리스너 객체의 메소드 호출 **/
+                        if (mListener != null) {
+                            mListener.onItemClick(view,pos);
+                            Log.d(this.getClass().getName(),"DB3");
+
+                            String dateStr = mData.get(pos).getDate();;
+                            Log.d("dateStr",""+dateStr);
+
+                            db = FirebaseFirestore.getInstance();
+                            db.collection(userId).document(dateStr)
+                                    .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(view.getContext(),CheckLetterActivity.class);
+                                        DocumentSnapshot documentSnapshot = task.getResult();
+                                        Log.d(this.getClass().getName(),"DB4 "+view.getContext());
+                                        if (documentSnapshot.exists()) {
+                                            Log.d(this.getClass().getName(),"DB안에 들어감");
+                                            intent.putExtra("dateStr",""+documentSnapshot.get("date"));
+                                            intent.putExtra("letterStr",""+documentSnapshot.get("letter"));
+                                            Log.d("dateStr",""+documentSnapshot.get("date"));
+
+                                            if (documentSnapshot.get("emoji").equals("good")) {
+                                                intent.putExtra("emojiStr","good");
+                                            } else if (documentSnapshot.get("emoji").equals("happy")) {
+                                                intent.putExtra("emojiStr","happy");
+                                            } else if (documentSnapshot.get("emoji").equals("angry")) {
+                                                intent.putExtra("emojiStr","angry");
+                                            } else if (documentSnapshot.get("emoji").equals("cry")) {
+                                                intent.putExtra("emojiStr","cry");
+                                            } else if (documentSnapshot.get("emoji").equals("sad")) {
+                                                intent.putExtra("emojiStr","sad");
+                                            } else if (documentSnapshot.get("emoji").equals("soso")) {
+                                                intent.putExtra("emojiStr","soso");
+                                            }
+
+                                        }
+
+                                        mContext.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                    }
+
+                                }
+                            });
+
+                        }
+                    }
+
+
+                }
+            });
 
 
         }
     }
 
-    public ViewLetterRecyclerAdapter (ArrayList<String> list){
-        this.mData=list;
+    public ViewLetterRecyclerAdapter (Context mContext, ArrayList<ItemDictionary> mData){
+        this.mData=mData;
+        this.mContext=mContext;
     }
 
     @NonNull
@@ -67,8 +151,7 @@ public class ViewLetterRecyclerAdapter extends RecyclerView.Adapter<ViewLetterRe
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        String text = mData.get(position);
-        holder.date.setText(text);
+        holder.kordate.setText(mData.get(position).getKordate());
     }
 
     @Override
@@ -76,5 +159,5 @@ public class ViewLetterRecyclerAdapter extends RecyclerView.Adapter<ViewLetterRe
 
         return (null != mData ? mData.size() : 0);
     }
-
+    
 }
